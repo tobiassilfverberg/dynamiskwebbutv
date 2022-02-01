f335545d099b126fc9efd28c474620f90d34c585
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-01 11:20:17 +0100

    __dirname and __filename

--- 01-basics/01-globals.js
+++ 01-basics/01-globals.js
@@ -4,6 +4,7 @@
 
 console.log("Hello world");
 
+/* 
 setTimeout(() => {
   console.log("I'll be back!");
 }, 3000);
@@ -13,3 +14,7 @@ setInterval(() => {
   counter += 2;
   console.log(`Hello after ${counter} seconds`);
 }, 2000);
+*/
+
+console.log("Absolute path to directory of this file:", __dirname);
+console.log("Absolute path to this file:", __filename);
