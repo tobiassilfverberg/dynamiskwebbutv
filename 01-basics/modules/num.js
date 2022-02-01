758fb4aed60cf11652d16ea80b9c26e8c8c57e51
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-01 15:21:27 +0100

    Fixed funtion to fat arrow funtion to shorten it

--- 01-basics/modules/num.js
+++ 01-basics/modules/num.js
@@ -1,8 +1,6 @@
 const roundWith1Dec = (num) => Math.round(num * 10) / 10;
 const roundWith2Dec = (num) => Math.round(num * 100) / 100;
-const roundWithPrecision = (num, precision) => {
-  return num.toFixed(precision);
-};
+const roundWithPrecision = (num, precision) => num.toFixed(precision);
 
 module.exports = {
   roundWith1Dec,
