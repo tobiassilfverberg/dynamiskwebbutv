100e917fa007f54a1ab046d08b40119523045aa2
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-01 13:21:21 +0100

    Path (basename, extname, parse)

--- 01-basics/03-path.js
+++ 01-basics/03-path.js
@@ -1,3 +1,19 @@
 /**
  * Path
  */
+
+console.log("Absolute path to directory of this file:", __dirname);
+console.log("Absolute path to this file:", __filename);
+
+const path = require("path");
+
+const file_wo_path = path.basename(
+  "/Applications/MAMP/htdocs/dynamiskwebbutv/01-basics/03-path.js"
+);
+console.log("Filename without path:", file_wo_path); // "03-path.js"
+
+const file_ext = path.extname(__filename);
+console.log("My extension is:", file_ext); // .js
+
+const parts = path.parse(__filename);
+console.log("All my parts:", parts); // root, dir, base, ext, name
