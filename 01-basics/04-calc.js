f6102556de66ecdd9a3157fcee4911669fc47ddf
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-01 13:59:29 +0100

    Module to calc area and circuference. Use module in calc-program

--- 01-basics/04-calc.js
+++ 01-basics/04-calc.js
@@ -1,3 +1,17 @@
 /**
  * Calc stuff
  */
+
+const geo = require("./modules/geometry");
+
+// console.log("geo", geo);
+
+let radius = 4;
+
+let area = geo.area(radius);
+let approxArea = Math.round(area * 10) / 10;
+console.log("the area is:", area);
+console.log("the approx. area is:", approxArea);
+
+let circumference = geo.circumference(radius);
+console.log("the circumference is:", circumference);
--- 01-basics/modules/geometry.js
+++ 01-basics/modules/geometry.js
@@ -1,3 +1,17 @@
 /**
  * Do all things geometry-related
  */
+
+const { PI } = Math;
+
+// n^2
+const area = (r) => PI * r ** 2;
+
+// n * 2
+const circumference = (r) => 2 * PI * r;
+
+// Export all the stuff
+module.exports = {
+  area,
+  circumference,
+};
