d68fe5e3443d7046f19f6294deb0fc1657a996a1
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-01 11:50:24 +0100

    readdir with callbacks and promises

--- 01-basics/02-fs.js
+++ 01-basics/02-fs.js
@@ -1,8 +1,8 @@
 /**
  * File system
- * 
+ *
  * commonly used methods are:
- * 
+ *
  * readdir() - read contents of a directory
  * readFile() - read a file
  * writeFile() - write to a file
@@ -11,3 +11,32 @@
  * rmdir() - remove a directory
  */
 
+/**
+ * readdir using callbacks
+ */
+/*
+const fs = require("fs");
+console.log("Before readdir...");
+
+// list contents in current directory
+fs.readdir(".", (err, files) => {
+  console.log("The contents in the current directory is:");
+  console.log(files);
+});
+
+console.log("After readdir...");
+*/
+
+/**
+ * readdir using promises
+ */
+const fs = require("fs").promises;
+
+fs.readdir(".")
+  .then((files) => {
+    console.log("The contents in the current directory is:");
+    console.log(files);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
