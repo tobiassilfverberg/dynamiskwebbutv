58158433d7912a92def1f17b1d7844f1e09071aa
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-01 11:00:14 +0100

    Read me file and additions to 01-globals file

--- 01-basics/01-globals.js
+++ 01-basics/01-globals.js
@@ -3,3 +3,13 @@
  */
 
 console.log("Hello world");
+
+setTimeout(() => {
+  console.log("I'll be back!");
+}, 3000);
+
+let counter = 0;
+setInterval(() => {
+  counter += 2;
+  console.log(`Hello after ${counter} seconds`);
+}, 2000);
---
+++ README.md
@@ -0,0 +1,1 @@
+Folder for the course "Dynamisk Webbutveckling"
