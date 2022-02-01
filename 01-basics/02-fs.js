fd036a56cf25faa44e83eb61be9626ba661fbcf4
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-01 11:30:53 +0100

    Commonly used methods

--- 01-basics/02-fs.js
+++ 01-basics/02-fs.js
@@ -1,3 +1,13 @@
 /**
  * File system
+ * 
+ * commonly used methods are:
+ * 
+ * readdir() - read contents of a directory
+ * readFile() - read a file
+ * writeFile() - write to a file
+ * unlink() - delete a file
+ * mkdir() - make a directory
+ * rmdir() - remove a directory
  */
+
