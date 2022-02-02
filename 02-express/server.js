181993f0e17f3806d5622b2575679f96440c93f1
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-02 13:20:15 +0100

    Second GET listener for /nom with different content.

--- 02-express/server.js
+++ 02-express/server.js
@@ -5,13 +5,20 @@
 const express = require('express');
 const app = express();
 
-// When a GET rquest for '/' (http://localhost:3000/) is received, run this function
+// Respond to GET rquest for '/'
 app.get('/', (req, res) => {
     // req = information om den inkommande förfrågan
     // res = metoder för att skicka ett svar på förfrågan
-    console.log("Someone requested my stuff");
-    console.log(req);
-    res.send("Hello from me");
+    console.log(req.method, req.url);
+
+    res.send("Hello from root");
+});
+
+// Respond to GET request for '/nom'
+app.get('/nom', (req, res) => {
+    console.log(req.method, req.url);
+
+    res.send("Cupcake ipsum dolor sit amet. Liquorice gingerbread powder gummies pudding cupcake fruitcake marshmallow chupa chups. Biscuit cake bonbon jelly beans apple pie chocolate bar soufflé.");
 });
 
 // Start listening for incoming requests on port 3000 
