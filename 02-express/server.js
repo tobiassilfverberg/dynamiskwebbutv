7d5b0d90f2538185003e01963640894f5a469ec8
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-02 13:36:10 +0100

    Respond to multiple requests

--- 02-express/server.js
+++ 02-express/server.js
@@ -21,6 +21,21 @@ app.get('/nom', (req, res) => {
     res.send("Cupcake ipsum dolor sit amet. Liquorice gingerbread powder gummies pudding cupcake fruitcake marshmallow chupa chups. Biscuit cake bonbon jelly beans apple pie chocolate bar soufflé.");
 });
 
+// Respond to GET request for '/about'
+app.get('/about', (req, res) => {
+    console.log(req.method, req.url);
+    res.set('Content-type', 'text/html');
+
+    res.write('<h1>About</h1>');
+    res.write('<p>This is the about page</p>');
+    res.end();
+});
+
+// Respond to GET request for `/api/nom`
+app.get('/api/nom', (req, res) => {
+    res.send({ msg: "Cakes are nom-nom-nom" });
+});
+
 // Start listening for incoming requests on port 3000 
 app.listen(3000, () => {
     console.log("Yay, server started at http://localhost:3000");
