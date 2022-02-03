b3bca226989a18f32f2fcac914b364b076fca824
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-03 13:43:59 +0100

    Workshop. Get array of jokes from json, parse them and res.send random one to website.

--- 02-express/server.js
+++ 02-express/server.js
@@ -3,42 +3,50 @@
  */
 
 const express = require('express');
+const fs = require("fs").promises;
 const path = require('path');
 const app = express();
 
-// Respond to GET rquest for '/'
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// // Respond to GET rquest for '/'
 app.get('/', (req, res) => {
     // req = information om den inkommande förfrågan
     // res = metoder för att skicka ett svar på förfrågan
-    console.log(req.method, req.url);
+    // console.log(req.method, req.url);
 
     res.send("Hello from root");
 });
 
-// Respond to GET request for '/nom'
-app.get('/nom', (req, res) => {
-    res.sendFile( path.join(__dirname + '/pages/nom.html'));
+// Respond with current time
+app.get('/now', (req, res) => {
+    res.send(`The current time is ${new Date()}`);
 });
 
-// respond to GET request for '/index'
-app.get('/index', (req, res) => {
-    res.sendFile(__dirname + '/pages/index.html');
-});
+ // Respond with a random oneliner joke 
+ app.get('/jokes', (req, res) => { 
+    // 1. Somehow read the JSON-contents of data/oneliners.json 
+    // 2. Get a random item from the array 
+    // 3. Respond with the item (`res.send(item)`) 
+    fs.readFile("./data/oneliners.json", "utf-8")
+    .then((data) => {
+        const jokes = JSON.parse(data);
+        console.log(jokes);
+        let numberOfJoke = getRandomInt(0, 7);
+        let randomJoke = jokes[numberOfJoke];
+        res.send(randomJoke);
+    })
+    .catch((e) => {
+        console.error(e);
+    });
+}); 
 
-// Respond to GET request for '/about'
-app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/pages/about.html');
-});
-
-// respond to GET request for '/contact'
-app.get('/contact', (req, res) => {
-    res.sendFile(__dirname + '/pages/contact.html');
-});
-
-// Respond to GET request for `/api/nom`
-app.get('/api/nom', (req, res) => {
-    res.send({ msg: "Cakes are nom-nom-nom" });
-});
+// Serve files from '/pages' if no other route matches
+app.use( express.static('pages') );
 
 // Start listening for incoming requests on port 3000 
 app.listen(3000, () => {
