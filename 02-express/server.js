cf0cec2f25d29abfff3f7f5e7a459e1757edf279
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-04 10:14:33 +0100

    Workshop with Johans solution

--- 02-express/server.js
+++ 02-express/server.js
@@ -3,17 +3,11 @@
  */
 
 const express = require('express');
-const fs = require("fs").promises;
+const oneliners = require('./data/oneliners.json');
 const path = require('path');
 const app = express();
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// // Respond to GET rquest for '/'
+// Respond to GET rquest for '/'
 app.get('/', (req, res) => {
     // req = information om den inkommande förfrågan
     // res = metoder för att skicka ett svar på förfrågan
@@ -31,18 +25,10 @@ app.get('/now', (req, res) => {
  app.get('/jokes', (req, res) => { 
     // 1. Somehow read the JSON-contents of data/oneliners.json 
     // 2. Get a random item from the array 
+    const i = Math.floor(Math.random() * oneliners.length);
+    const oneliner = oneliners[i];
     // 3. Respond with the item (`res.send(item)`) 
-    fs.readFile("./data/oneliners.json", "utf-8")
-    .then((data) => {
-        const jokes = JSON.parse(data);
-        console.log(jokes);
-        let numberOfJoke = getRandomInt(0, 7);
-        let randomJoke = jokes[numberOfJoke];
-        res.send(randomJoke);
-    })
-    .catch((e) => {
-        console.error(e);
-    });
+    res.send(oneliner);
 }); 
 
 // Serve files from '/pages' if no other route matches
