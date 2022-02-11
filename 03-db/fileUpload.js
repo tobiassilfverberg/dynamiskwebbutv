22961932d49a1316e79ed3a15d1444b1fedbfce0
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-11 11:36:26 +0100

    Uploading files and saving them using Multer bodyparser and setting a new filename

--- 03-db/fileUpload.js
+++ 03-db/fileUpload.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const app = express();
+const multer = require("multer");
 
 app.use(express.static("static"));
+/* 
+const urlencoded = express.urlencoded({ extended: false });
+const jsonencoder = express.json();
+app.use(urlencoded);
+*/
+const storageObject = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const strDate = new Date().valueOf().toString();
+    cb(null, strDate + "-" + file.originalname);
+  },
+});
+
+const multipartdataEncoder = multer({ storage: storageObject });
+
+app.post("/upload", multipartdataEncoder.single("myFile"), (req, res) => {
+  console.log(req.file);
+  res.send(req.file);
+});
 
 app.listen(3000, () => {
   console.log("Server started att http://localhost:3000");
