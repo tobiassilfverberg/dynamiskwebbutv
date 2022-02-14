4dfb36cd3722805de737ebefce21f716fd29ef3f
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-14 17:32:13 +0100

    Fixed syntax error on .toJSON()

--- 03-db/bibliotekCon.js
+++ 03-db/bibliotekCon.js
@@ -2,9 +2,9 @@ const Authors = require("./Authors");
 const Titles = require("./Titles");
 
 Authors.fetchAll().then((authors) => {
-  console.log(authors.ToJSON);
+  console.log(authors.toJSON());
 });
 
-Titles.fetchAll().then((titles) => {
-  console.log(titles.ToJSON);
-});
+/* Titles.fetchAll().then((titles) => {
+  console.log(titles.toJSON());
+}); */
