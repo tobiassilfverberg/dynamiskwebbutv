9392724dcaa0a31dd3da5798a1e0c0f5cc6f43ed
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-15 10:28:53 +0100

    Workshop, implement routes for pokemonfriends and pokemonbattles

--- 04-pokemon/routes/index.js
+++ 04-pokemon/routes/index.js
@@ -7,4 +7,8 @@ router.get("/", (req, res) => {
 
 router.use("/pokemoncards", require("./pokemoncards_route"));
 
+router.use("/pokemonfriends", require("./pokemoncards_route"));
+
+router.use("/pokemonbattles", require("./pokemoncards_route"));
+
 module.exports = router;
