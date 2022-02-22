9df2e057a3c7450531d6d3162fde3ec845a7746b
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-22 14:33:35 +0100

    Optional validator to make sure the info from user is correct

--- 06-books/app.js
+++ 06-books/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
+const { body, matchedData, validationResult } = require("express-validator");
 
 // instantiate express
 const app = express();
@@ -10,6 +11,24 @@ app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.post(
+  "/test",
+  [
+    body("name").exists().isLength({ min: 3 }),
+    body("address").exists().isString().trim().isLength({ min: 6, max: 50 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).send({ status: "fail", data: errors.array() });
+    }
+
+    const validData = matchedData(req);
+
+    res.send({ status: "success", data: validData });
+  }
+);
 
 // routes
 app.use(require("./routes"));
