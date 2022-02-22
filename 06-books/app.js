28816e9abf423550718f1f962d6feff7d4aae97d
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-22 13:16:08 +0100

    Changes to code and removed som parts not needed.

--- 06-books/app.js
+++ 06-books/app.js
@@ -1,21 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
 
 // instantiate express
 const app = express();
 
 // middlewares
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(require("./routes"));
 
 module.exports = app;
