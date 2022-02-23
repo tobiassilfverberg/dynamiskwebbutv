523d0b4b882dd015bd8ead8c6d979f0b2dd0c905
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-02-23 09:23:14 +0100

    Updated validator rules with promises to check if input already exists

--- 06-books/validation/book.js
+++ 06-books/validation/book.js
@@ -7,14 +7,36 @@ const createRules = [
   body("title").exists().isString().isLength({ min: 1 }),
   body("isbn").optional().isString().isLength({ min: 8 }),
   body("pages").exists().isInt().isLength({ min: 1 }),
-  body("author_id").exists().isInt().isLength({ min: 1 }),
+  body("author_id")
+    .exists()
+    .custom(async (value) => {
+      const author = await new models.Author({ id: value }).fetch({
+        require: false,
+      });
+      if (!author) {
+        return Promise.reject(`Author with ID ${value} does not exist.`);
+      }
+
+      return Promise.resolve();
+    }),
 ];
 
 const updateRules = [
   body("title").optional().isString().isLength({ min: 1 }),
   body("isbn").optional().isString().isLength({ min: 8 }),
   body("pages").optional().isInt().isLength({ min: 1 }),
-  body("author_id").optional().isInt().isLength({ min: 1 }),
+  body("author_id")
+    .optional()
+    .custom(async (value) => {
+      const author = await new models.Author({ id: value }).fetch({
+        require: false,
+      });
+      if (!author) {
+        return Promise.reject(`Author with ID ${value} does not exist.`);
+      }
+
+      return Promise.resolve();
+    }),
 ];
 
 module.exports = {
--- 06-books/validation/user.js
+++ 06-books/validation/user.js
@@ -4,10 +4,22 @@
 const { body } = require("express-validator");
 
 const createRules = [
-  body("username").exists().isLength({ min: 3 }),
-  body("password").exists().isLength({ min: 6 }),
-  body("first_name").exists().isString().isLength({ min: 2 }),
-  body("last_name").exists().isString().isLength({ min: 2 }),
+  body("username")
+    .exists()
+    .isLength({ min: 3 })
+    .custom(async (value) => {
+      const user = await new models.User({ username: value }).fetch({
+        require: false,
+      });
+      if (user) {
+        return Promise.reject("Username already exists.");
+      }
+
+      return Promise.resolve();
+    }),
+  body("password").exists().isLength({ min: 4 }),
+  body("first_name").exists().isLength({ min: 2 }),
+  body("last_name").exists().isLength({ min: 2 }),
 ];
 
 // Allow everything but password to be updated
