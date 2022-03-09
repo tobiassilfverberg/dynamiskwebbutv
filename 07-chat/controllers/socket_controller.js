f4f8eaadaaa6ff0fafd3906b2ba80a85b1dde90c
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-03-09 13:57:50 +0100

    Styling notices and messages differently, as well as displaying the name of whoever joins

--- 07-chat/controllers/socket_controller.js
+++ 07-chat/controllers/socket_controller.js
@@ -4,17 +4,35 @@
 
 const debug = require("debug")("chat:socket_controller");
 
+// list of socket-ids and their username
+const users = {};
+
 module.exports = function (socket) {
 	debug("a new client has connected", socket.id);
 
-	// broadcast that a new user has connected
-	socket.broadcast.emit("user:connected");
-
 	// handle user disconnect
 	socket.on("disconnect", function () {
-		debug(`Client ${this.id} disconnected :(`);
+		debug(`Client ${socket.id} disconnected :(`);
 
-		this.broadcast.emit("user:disconnected");
+		// let everyone connected know that user disconnected
+		this.broadcast.emit("user:disconnected", users[socket.id]);
+
+		// remove user from list of connected user
+		delete users[socket.id];
+	});
+
+	// handle user joined
+	socket.on("user:joined", function (username, callback) {
+		// associate socket id with username
+		users[socket.id] = username;
+
+		// let everyone know that someone has connected to the chat
+		socket.broadcast.emit("user:connected", username);
+
+		// confirm join
+		callback({
+			success: true,
+		});
 	});
 
 	// handle user emitting a new message
--- 07-chat/public/assets/js/chat.js
+++ 07-chat/public/assets/js/chat.js
@@ -1,93 +1,108 @@
 const socket = io();
 
-const startEl = document.querySelector('#start');
-const chatWrapperEl = document.querySelector('#chat-wrapper');
-const usernameForm = document.querySelector('#username-form');
-const messagesEl = document.querySelector('#messages'); // ul element containing all messages
-const messageForm = document.querySelector('#message-form');
-const messageEl = document.querySelector('#message');
+const startEl = document.querySelector("#start");
+const chatWrapperEl = document.querySelector("#chat-wrapper");
+const usernameForm = document.querySelector("#username-form");
+const messagesEl = document.querySelector("#messages"); // ul element containing all messages
+const messageForm = document.querySelector("#message-form");
+const messageEl = document.querySelector("#message");
 
 let username = null;
 
 const addMessageToChat = (message, ownMsg = false) => {
 	// create new `li` element
-	const liEl = document.createElement('li');
+	const liEl = document.createElement("li");
 
 	// set class of `li` to `message`
-	liEl.classList.add('message');
-
-	// set content of `li` element
-	liEl.innerText = message;
+	liEl.classList.add("message");
 
 	if (ownMsg) {
-		liEl.classList.add('you');
+		liEl.classList.add("you");
 	}
+
+	// set content of `li` element
+	liEl.innerHTML = ownMsg
+		? msg.content
+		: `<span class="user">${msg.username}</span: ${msg.content}`;
 
 	// append `li` element to `#messages`
 	messagesEl.appendChild(liEl);
 
 	// scroll `li` element into view
 	liEl.scrollIntoView();
-}
+};
 
-const addNoticeToChat = notice => {
-	const liEl = document.createElement('li');
-	liEl.classList.add('notice');
+const addNoticeToChat = (notice) => {
+	const liEl = document.createElement("li");
+	liEl.classList.add("notice");
 
 	liEl.innerText = notice;
 
 	messagesEl.appendChild(liEl);
 	liEl.scrollIntoView();
-}
+};
 
 // listen for when a new user connects
-socket.on('user:connected', () => {
-	addNoticeToChat("Someone connected");
+socket.on("user:connected", (username) => {
+	addNoticeToChat(username + " connected");
 });
 
 // listen for when a user disconnects
-socket.on('user:disconnected', () => {
-	addNoticeToChat("Someone disconnected");
+socket.on("user:disconnected", (username) => {
+	addNoticeToChat(username + " disconnected");
 });
 
 // listen for incoming messages
-socket.on('chat:message', message => {
+socket.on("chat:message", (message) => {
 	console.log("Someone said something:", message);
 
 	addMessageToChat(message);
 });
 
 // get username from form and show chat
-usernameForm.addEventListener('submit', e => {
+usernameForm.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	username = usernameForm.username.value;
 
-	// hide start view
-	startEl.classList.add('hide');
+	// emit `user:joined event and when we get acknowledgement, THEN show chat
+	socket.emit("user:joined", username, (status) => {
+		// we've received acknowledgement from server
+		console.log("Server acknowledged that user joined", status);
 
-	// show chat view
-	chatWrapperEl.classList.remove('hide');
+		if (status.success) {
+			// hide start view
+			startEl.classList.add("hide");
 
-	// focus on inputMessage
-	messageEl.focus();
+			// show chat view
+			chatWrapperEl.classList.remove("hide");
+
+			// focus on inputMessage
+			messageEl.focus();
+		}
+	});
 });
 
 // send message to server
-messageForm.addEventListener('submit', e => {
+messageForm.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	if (!messageEl.value) {
 		return;
 	}
 
+	const msg = {
+		username,
+		content: messageEl.value,
+	};
+
 	// send message to server
-	socket.emit('chat:message', messageEl.value);
+	socket.emit("chat:message", msg);
 
 	// add message to chat
-	addMessageToChat(messageEl.value, true);
+	addMessageToChat(msg, true);
 
 	// clear message input element and focus
-	messageEl.value = '';
+	messageEl.value = "";
 	messageEl.focus();
 });
