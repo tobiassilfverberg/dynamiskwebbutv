d023d0e9f9f909a44af02cf8be120da2c84b1935
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-03-10 14:48:03 +0100

    Updated fetchById in User models aswell

--- photoAPI/models/User.js
+++ photoAPI/models/User.js
@@ -37,8 +37,8 @@ module.exports = (bookshelf) => {
         return user;
       },
 
-      async fetchById(id) {
-        const user = await new this({ id }).fetch({ require: false });
+      async fetchById(id, fetchOptions) {
+        const user = await new this({ id }).fetch(fetchOptions);
         if (!user) {
           return false;
         }
