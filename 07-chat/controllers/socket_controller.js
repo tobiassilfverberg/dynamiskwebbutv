ac0fcf1aee2fadbe411a31b6a7fa28fa22042af9
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-03-11 10:56:06 +0100

    Code up to date

--- 07-chat/controllers/socket_controller.js
+++ 07-chat/controllers/socket_controller.js
@@ -4,42 +4,59 @@
 
 const debug = require("debug")("chat:socket_controller");
 
+let io = null; // socket.io server instance
+let socket = null; // socket to the client
+
 // list of socket-ids and their username
 const users = {};
 
-module.exports = function (socket) {
+const handleDisconnect = function () {
+	debug(`Client ${socket.id} disconnected :(`);
+
+	// let everyone connected know that user has disconnected
+	socket.broadcast.emit("user:disconnected", users[socket.id]);
+
+	// remove user from list of connected users
+	delete users[socket.id];
+};
+
+// Handle when a user has joined the chat
+const handleUserJoined = function (username, callback) {
+	// associate socket id with username
+	users[socket.id] = username;
+
+	debug(`User ${username} with socket id ${socket.id} joined`);
+
+	// let everyone know that someone has connected to the chat
+	socket.broadcast.emit("user:connected", username);
+
+	// confirm join
+	callback({
+		success: true,
+	});
+};
+
+const handleChatMessage = function (message) {
+	debug("Someone said something: ", message);
+
+	// emit `chat:message` event to everyone EXCEPT the sender
+	socket.broadcast.emit("chat:message", message);
+};
+
+module.exports = function (_socket, _io) {
+	io = _io;
+	socket = _socket;
+
 	debug("a new client has connected", socket.id);
 
+	io.emit("new-connection", "A new user connected");
+
 	// handle user disconnect
-	socket.on("disconnect", function () {
-		debug(`Client ${socket.id} disconnected :(`);
-
-		// let everyone connected know that user disconnected
-		this.broadcast.emit("user:disconnected", users[socket.id]);
-
-		// remove user from list of connected user
-		delete users[socket.id];
-	});
+	socket.on("disconnect", handleDisconnect);
 
 	// handle user joined
-	socket.on("user:joined", function (username, callback) {
-		// associate socket id with username
-		users[socket.id] = username;
-
-		// let everyone know that someone has connected to the chat
-		socket.broadcast.emit("user:connected", username);
-
-		// confirm join
-		callback({
-			success: true,
-		});
-	});
+	socket.on("user:joined", handleUserJoined);
 
 	// handle user emitting a new message
-	socket.on("chat:message", function (message) {
-		debug("Someone said something: ", message);
-
-		// emit `chat:message` event to everyone EXCEPT the sender
-		this.broadcast.emit("chat:message", message);
-	});
+	socket.on("chat:message", handleChatMessage);
 };
